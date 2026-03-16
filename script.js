db5e5d1ed25baf68e9d04f566f34c60e4acbbc07
narayananv6525-source <narayananv6525@gmail.com>
2026-03-16 11:11:58 +0530

    Pushing my portfolio 16.03.2026

---
+++ script.js
@@ -0,0 +1,54 @@
+// ---------- TAB SECTION (About section tabs) ----------
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+
+    for(var tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+
+    for(var tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+// ---------- MOBILE MENU ----------
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+
+
+// ---------- CONTACT FORM (Google Sheets) ----------
+
+const scriptURL = "https://script.google.com/macros/s/AKfycbzECwzlFnTvh_meS3IbJ0fbyFDGKZ8nS8mf0UGD5-54Uk1wY8atpbayK6qPFROtAUdT/exec";
+
+const form = document.forms["submit-to-google-sheet"];
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    fetch(scriptURL, {
+        method: "POST",
+        body: new FormData(form)
+    })
+    .then(response => {
+        alert("Message sent successfully!");
+        form.reset();
+    })
+    .catch(error => {
+        alert("Something went wrong. Please try again.");
+    });
+});
